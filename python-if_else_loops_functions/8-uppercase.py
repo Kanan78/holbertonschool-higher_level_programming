2b0e309340453993ba6438ee6f0a189c9f5e86d5
kanan78 <kmusayev27@gmail.com>
2025-11-15 16:32:36 +0000

    commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
     for i in str:
-        if 97 <= ord(i) <= 122:  # 'a'–'z' aralığı
-            result += chr(ord(i) - 32)
+        if 97 <= ord(i) <= 122:
+            print("{}".format(chr(ord(i) - 32)), end="")
         else:
-            result += i
-    print("{}".format(result), end="")
+            print("{}".format(i), end="")
