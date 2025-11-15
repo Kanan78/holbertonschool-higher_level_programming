50d2eb3234b6c92c9ec996c2a1955ab37c9e5c52
kanan78 <kmusayev27@gmail.com>
2025-11-15 16:03:53 +0000

    commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if 97 <= ord(c) <= 122:
-        print("{} is lower".format(c))
+        return True
     else:
-        print("{} is upper".format(c))
+        return False
