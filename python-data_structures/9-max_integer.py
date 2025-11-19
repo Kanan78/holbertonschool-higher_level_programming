9002a7d91513f006281824f25f242edf5ddc4f51
kanan78 <kmusayev27@gmail.com>
2025-11-19 09:51:17 +0000

    commit

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list):
+    if len(my_list) == 0:
+        return None
+    n = my_list[0]
+    for element in my_list:
+        if element > n:
+            n = element
+    return n
