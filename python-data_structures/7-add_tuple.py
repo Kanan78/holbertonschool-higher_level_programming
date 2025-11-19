cbc3da6ef034f59c97bfa8e2b85564244cdf1067
kanan78 <kmusayev27@gmail.com>
2025-11-19 09:25:57 +0000

    commit

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a, tuple_b):
+    m = ()
+    for i in range(0,len(tuple_a)):
+        for j in range(0,len(tuple_b)):
+            m[i] = tuple_a[i] + tuple_b[i]
+    return m
