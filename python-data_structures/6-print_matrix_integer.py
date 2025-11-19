6432bb2a65467c62528c4306342aac0c6d80fde7
kanan78 <kmusayev27@gmail.com>
2025-11-19 09:14:12 +0000

    commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
     for row in matrix:
-        for idx,element in enumerate(row):
+        for idx, element in enumerate(row):
             if idx == len(row) - 1:
                 print("{:d}".format(element), end="")
             else:
