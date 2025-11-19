4ce22f12ea1a17c457370df2ade6f1874a289fa0
kanan78 <kmusayev27@gmail.com>
2025-11-19 09:01:30 +0000

    commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix):
     for i in range(0,len(matrix)):
         for j in range(0,len(matrix)):
-            if i < len(matrix) - 1:
+            if i <= len(matrix) - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
                 print("{:d}".format(matrix[i][j]), end="")
