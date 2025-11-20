8dede5b7ae23931ad21d268431b335e367ba791d
kanan78 <kmusayev27@gmail.com>
2025-11-20 14:02:23 +0000

    commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix):
     new_matrix = []
-    for i in range(len(matrix)):
+    for i in matrix:
         new_matrix += map(lambda k: k**2, i)
     return list(new_matrix)
