e600878dd8913b79963e9068fde1d8a7c14cc504
kanan78 <kmusayev27@gmail.com>
2025-11-20 14:44:55 +0000

    commit

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     c_set = {}
-    for i in set_1:
-        if i in set_2:
-            c_set.append(i)
+    for v in set_1:
+        if v in set_2:
+            c_set.add(v)
