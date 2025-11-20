e053951c60afcb770c36b4e3e28cedb92cff5403
kanan78 <kmusayev27@gmail.com>
2025-11-20 14:15:28 +0000

    commit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i,v in enumerate(my_list):
+    for i, v in enumerate(my_list):
         if v == search:
             new_list.append(replace)
         else:
             new_list.append(v)
-    return new_list 
+    return new_list
