85c30cd6b23ca7d82a239acb03c6d96b32842e73
kanan78 <kmusayev27@gmail.com>
2025-11-22 15:11:46 +0000

    commit

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    c_set = set()
-    for v in set_1:
-        if not(v in set_2):
-            c_set.add(v)
+    c_set = set_1 ^ set_2
     return c_set
