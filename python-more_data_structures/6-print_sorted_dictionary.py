5e5b2292a3a3d7a0dbbf62adcc34ae8dc58f049d
kanan78 <kmusayev27@gmail.com>
2025-11-22 15:41:00 +0000

    commit

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     a_dictionary = dict(sorted(a_dictionary.items()))
-    for i,v in a_dictionary.items():
+    for i, v in a_dictionary.items():
         print(f"{i}: {v}")
