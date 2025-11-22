5cd219b20c96a8d5e8fa8e3833eb1e0af17afea9
kanan78 <kmusayev27@gmail.com>
2025-11-22 18:09:16 +0000

    commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list, x):
     try:
+        k = 0
         for i in range(x):
+            k += 1
             print(my_list[i], end='')
         print(f"\n{x}")
     except IndexError:
-        k = 0
-        for i in my_list:
-            print(i,end='')
-            k += 1 
-        print(f"\n{x}")
-
+        pass
