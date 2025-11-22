dfa5825a264577dc3b43aa7263e59e745b238898
kanan78 <kmusayev27@gmail.com>
2025-11-22 18:40:54 +0000

    commit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
+            k += 1
         except (TypeError,ValueError):
-            pass
-        k += 1 
+            pass 
     print()
-    return x
+    return k
     
