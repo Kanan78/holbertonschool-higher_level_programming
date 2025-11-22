0775651843ad2dbb04a522c4bbb1bf7d620bd56a
kanan78 <kmusayev27@gmail.com>
2025-11-22 15:55:45 +0000

    commit

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i,v in a_dictionary.items():
+    for i, v in a_dictionary.items():
         a_dictionary[i] = v*2
     return a_dictionary
