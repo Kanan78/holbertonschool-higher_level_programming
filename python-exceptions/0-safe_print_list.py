a96a2ffe5b931a80d6ba8db78639ce709be45953
kanan78 <kmusayev27@gmail.com>
2025-11-22 18:14:22 +0000

    commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list, x):
     try:
         k = 0
-        for i in range(x)
+        for i in range(x):
             print(my_list[i], end='')
             k += 1
     except IndexError:
