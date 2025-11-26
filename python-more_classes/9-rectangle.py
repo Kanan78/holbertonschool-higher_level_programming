742742b370d13c0c7fa3b66dcbbb1420ef7f5c9c
kanan78 <kmusayev27@gmail.com>
2025-11-26 13:58:21 +0000

    commit

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         if size < 0:
-            raise raise ValueError("Width Must be >= 0")
+            raise ValueError("Width Must be >= 0")
         return cls(size,size)
 
     @property
