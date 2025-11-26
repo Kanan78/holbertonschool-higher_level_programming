a4c4809dff92ef77218a8fbfa433b30eb6851d7d
kanan78 <kmusayev27@gmail.com>
2025-11-26 12:20:24 +0000

    commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,29 +5,29 @@
 class Rectangle:
     """This class defines a rectangle by: (based on 0-rectangle.py)"""
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         if isinstance(value, int):
-            if width < 0:
+            if self.__width < 0:
                 raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         if isinstance(value, int):
-            if height < 0:
+            if self.__height < 0:
                 raise ValueError("height must be >= 0")
         else:
             raise TypeError("height must be an integer")
