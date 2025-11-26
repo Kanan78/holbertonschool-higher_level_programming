4f9ef7d020c7a9822f17ec9edfa7f3869202b174
kanan78 <kmusayev27@gmail.com>
2025-11-26 13:35:01 +0000

    commit

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -49,8 +49,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         sy = getattr(self, "print_symbol", Rectangle.print_symbol)
-        return "\n".join([str(sy) * self.__width for _ in range(self.__height)])
-
+        lines = [str(sy) * self.__width for _ in range(self.__height)]
+        return "\n".join(lines)
     def __repr__(self):
         return (f"Rectangle({self.__width}, {self.__height})")
 
