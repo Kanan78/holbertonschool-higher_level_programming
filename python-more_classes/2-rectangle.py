b2d8eb48a9c4982f82ff901410a3a5e69225c150
kanan78 <kmusayev27@gmail.com>
2025-11-26 12:48:50 +0000

    commit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -39,13 +39,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2*(self.__width + self.__height)
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        a = ""
-        for i in range(self.__height):
-            a += ("#" * self.__width)
-            if i != self.__height - 1:
-                a += "\n"
-        return a
