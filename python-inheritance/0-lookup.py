9d4e89965dcd0a8c0e79e80bb86c6d8ddc3b092c
kanan78 <kmusayev27@gmail.com>
2025-11-27 13:34:41 +0000

    commit

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""This module defines lookup function"""
+
+
 def lookup(obj):
+    """This method returns the list of available attributes and methods"""
+
     return dir(obj)
