94c96afc558a048296a8c554b551c446f0840771
kanan78 <kmusayev27@gmail.com>
2025-11-29 19:15:13 +0000

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,11 +6,11 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-with open("add_item.json", 'r') as f:
-    try:
+try:
+    with open("add_item.json", 'r') as f:
         current_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        current_list = []
+except FileNotFoundError:
+    current_list = []
 
-    current_list.extend(sys.argv[1:])
-    save_to_json_file(current_list, "add_item.json")
+current_list.extend(sys.argv[1:])
+save_to_json_file(current_list, "add_item.json")
