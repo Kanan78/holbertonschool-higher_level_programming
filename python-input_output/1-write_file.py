65a5a13d2c5b654969465de9d056ffd1481d97cd
kanan78 <kmusayev27@gmail.com>
2025-11-29 17:53:19 +0000

    commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This module defines the function write_file"""
+
+
 def write_file(filename, text):
+    """This function writes the text to the file"""
     with (filename, 'w') as f:
         return f.write(text)
