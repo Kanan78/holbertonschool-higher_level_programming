633abb61f3e7f9beb3e5ad7592067832d4d498a7
kanan78 <kmusayev27@gmail.com>
2025-11-29 14:38:53 +0000

    commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines function is_same_class"""
+
+
 def is_same_class(obj, a_class):
+    """This function checks an existence of the object in the class"""
+
     if isinstance(obj, a_class):
         return True
     return False
