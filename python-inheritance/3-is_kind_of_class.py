adb7c82ef4ba9cdbcc0e1ff4a53781607875d13d
kanan78 <kmusayev27@gmail.com>
2025-11-29 14:49:46 +0000

    commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines the funcion 3-is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This function defines if the object is an instance of the class"""
+    return isinstance(obj, a_class)
