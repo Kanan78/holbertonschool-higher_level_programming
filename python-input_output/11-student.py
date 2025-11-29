13ed3877b9f78637aa7c803a64e4c9e4247c9b05
kanan78 <kmusayev27@gmail.com>
2025-11-29 20:15:53 +0000

    commit

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module defines the class Student"""
+
+
+class Student:
+    """This class defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        result = {}
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+            return result
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i, v in json.items():
+            self.i = v
