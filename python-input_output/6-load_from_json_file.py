5956d9dfbd12e6c2bf3126dffa2a78435277ecb0
kanan78 <kmusayev27@gmail.com>
2025-11-29 18:37:13 +0000

    commit

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 
 def save_to_json_file(filename):
-    """This function writes an object to a text file"""
+    """This function creates an object from a JSON file"""
     with open(filename, 'r') as f:
         json.load(f)
