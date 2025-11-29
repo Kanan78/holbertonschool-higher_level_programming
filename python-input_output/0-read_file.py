e0d84e7fc4c21f9ec4f21d7a56971ea365db8cb2
kanan78 <kmusayev27@gmail.com>
2025-11-29 21:24:55 +0000

    commit

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the function read_file"""
+
+
+def read_file(filename=""):
+    """This function reads a text file and prints it to stdout"""
+    with open(filename, 'r') as f:
+        t = f.read()
+        print(t)
