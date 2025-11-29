4aaf19b75e60f3f5daeb8806ae2ba0ebc534d146
kanan78 <kmusayev27@gmail.com>
2025-11-29 15:04:13 +0000

    commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class defines basic geometry formulas"""
+    def area(self):
+        raise Exception("area() is not implemented")
