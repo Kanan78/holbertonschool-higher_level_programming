3b03553bd71b2109adf7959d1e6b3b6b1c1b4273
kanan78 <kmusayev27@gmail.com>
2025-11-30 14:38:05 +0000

    commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- It creates the MySQL server user user_0d_1
 CREATE USER if NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
