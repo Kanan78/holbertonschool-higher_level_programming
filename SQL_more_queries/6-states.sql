c330360d6fde843c87907afa1e88158c075ac92e
kanan78 <kmusayev27@gmail.com>
2025-11-30 15:07:41 +0000

    commit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- It creates the database hbtn_0d_usa and the table states( in the database hbtn_0d_usa) on server
 CREATE database if NOT EXISTS hbtn_0d_usa
-CREATE table if NOT EXISTS states (
-	id int UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE table if NOT EXISTS hbtn_0d_usa.states (
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name varchar(256)
 );
