3cbb2d1c634a425974b78882421b74e575a4a84f
kanan78 <kmusayev27@gmail.com>
2025-11-30 13:20:39 +0000

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,13 @@
 -- It ptints description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT
+    TABLE_NAME AS 'Table',
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+  AND TABLE_NAME = 'first_table';
 
