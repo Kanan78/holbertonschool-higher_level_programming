f293fb1b481cb4b6d446189127cb7dc6a3f34957
kanan78 <kmusayev27@gmail.com>
2025-11-30 14:55:37 +0000

    commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,12 @@
--- It creates the database hbtn_0d_2 and the user user_0d_2
-CREATE database if NOT EXISTS hbtn_0d_2;
-CREATE user if NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_02`.* TO 'user_0d_2'@'localhost';
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on the database to the user
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
+-- Apply changes
 FLUSH PRIVILEGES;
+
