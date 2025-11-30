9c956ee16418ca9d52d03793645e17c3ff4e7be5
kanan78 <kmusayev27@gmail.com>
2025-11-30 14:53:47 +0000

    commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- It creates the database hbtn_0d_2 and the user user_0d_02
+-- It creates the database hbtn_0d_2 and the user user_0d_2
 CREATE database if NOT EXISTS hbtn_0d_2;
-CREATE user if NOT EXISTS 'user_0d_02'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_02 TO 'user_0d_02'@'localhost';
+CREATE user if NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_02`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
