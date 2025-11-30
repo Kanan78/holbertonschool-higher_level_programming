615d13b334b6c4904e7b0bcfe1545900a4408a2e
kanan78 <kmusayev27@gmail.com>
2025-11-30 14:32:22 +0000

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- It lists of all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW grants FOR 'user_0d_1@localhost';
-SHOW grants FOR 'user_0d_2@localhost';
+SHOW grants FOR 'user_0d_1'@'localhost';
+SHOW grants FOR 'user_0d_2'@'localhost';
