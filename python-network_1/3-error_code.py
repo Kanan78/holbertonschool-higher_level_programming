b2c08716a2944c785437c4feb61d30ea46ef65ec
kanan78 <kmusayev27@gmail.com>
2025-12-03 12:24:03 +0000

    commit

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""It takes in a url, sends a request to the url and displays the response"""
+
+import urllib.request
+import sys
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            print(data)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
