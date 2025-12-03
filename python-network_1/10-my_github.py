1521287ea55836b2417e4e08da6dabcf9f064d4e
kanan78 <kmusayev27@gmail.com>
2025-12-03 13:50:11 +0000

    commit

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -4,10 +4,11 @@
 import requests
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-url = "https://api.github.com/user"
-r = requests.get(url, auth=(username,password))
-data = r.json()
-print(data.get("id"))
+if len(sys.argv) == 3:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(username,password))
+    data = r.json()
+    print(data.get("id"))
 
