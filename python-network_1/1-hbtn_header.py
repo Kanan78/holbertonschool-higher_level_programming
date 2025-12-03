4f785834d12aea85db8a4f3f46c8f88cec8d7332
kanan78 <kmusayev27@gmail.com>
2025-12-03 11:58:13 +0000

    commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,6 +3,9 @@
 import sys
 import urllib.request
 
+if len(sys.argv) < 2:
+    sys.exit(1)
+
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     x_request_id = response.getheader("X-Request-Id")
