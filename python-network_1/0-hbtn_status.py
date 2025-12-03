1e951795ba86f6999b3739095cd7b178a822dbf4
kanan78 <kmusayev27@gmail.com>
2025-12-03 11:37:46 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,5 +3,5 @@ import urllib.request
 
 url = "https://intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
-    data = response.read.decode("utf-8")
+    data = response.read().decode("utf-8")
 print(data)
