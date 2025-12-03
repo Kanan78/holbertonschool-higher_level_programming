f1ed00f0e7d397ae5a9f79952313bf027f31a403
kanan78 <kmusayev27@gmail.com>
2025-12-03 13:40:26 +0000

    commit

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""It takes your GitHub credentials and display your id"""
+
+import requests
+import sys
+
+if len(sys.argv) == 3:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(username,password))
+    try:
+        data = r.json()
+        print(data.get("id"))
+    except ValueError:
+        print("None")
+
