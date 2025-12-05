ab6d88540204f25ef770dab2be4b50bc24834540
kanan78 <kmusayev27@gmail.com>
2025-12-05 23:32:34 +0000

    commit

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,13 +7,11 @@ import json
 
 class MyAPI(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-
-        self.send_header("Content-Type", "text/plain")
-
-        self.end_headers()
-
-        self.wfile.write(b"Hello, this is a simple API!")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
 
         if self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
@@ -24,6 +22,13 @@ class MyAPI(BaseHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(json_data)
+        
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(b'{"status": "OK"}')
 
         elif self.path == "/info":
             info = {
