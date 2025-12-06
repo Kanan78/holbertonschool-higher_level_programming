0a89122ee494170f1ec517005a8fac447ece0e95
kanan78 <kmusayev27@gmail.com>
2025-12-06 23:30:07 +0000

    commit

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from flask import Flask
+import csv
+import json
+
+app = Flask(__name__)
+
+def read_json():
+    try:
+        with open("products.json", "r") as file:
+            return json.load(file)
+    except:
+        return []
+
+
+def read_csv():
+    data = []
+    try:
+        with open("products.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append({
+                    "id": int(row["id"]),
+                    "name": row["name"],
+                    "category": row["category"],
+                    "price": float(row["price"])
+                        })
+        return data
+    except Exception:
+        return []
+
+@app.route("/products")
+def products_route():
+    source = request.args.get("source")
+    product_id = requests.args.get("id")
+    error = None
+    products = []
+
+    if source not in ["json", "csv"]:
+        return render_template("product_display.html", error="Wrong source", products=None)
+
+    products = read_json() if source == "json" else read_csv()
+
+
+    if product_id:
+        try:
+            pid = int(product_id)
+            filtered = [p for p in products if p["id"] == pid]
+
+            if not filtered:
+                return render_template("product_display.html", error="Product not found", products=None)
+
+            products = filtered
+
+        except ValueError:
+            return render_template("product_display.html", error="Invalid ID", products=None)
+
+    return render_template("product_display.html", error=None, products=products)
+
+
+
+if __name__ == "__main__":
+    app.run()
